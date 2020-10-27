6dbd9c4e5cbc167219c5be2851a7893d6c32efa1
Josse Lardinois <437980@student.fontys.nl>
2020-10-27 14:45:04 +0100

    Alle juiste code in juiste lagen

--- ApexLegendsApp/Controllers/CalculatorController.cs
+++ ApexLegendsApp/Controllers/CalculatorController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ApexLogic;
 
 namespace ApexLegendsApp.Controllers
 {
@@ -14,50 +15,14 @@ namespace ApexLegendsApp.Controllers
         {
             return View(new CalculatorModel());
         }
+
         [HttpPost]
-        public ActionResult PackCalculator(CalculatorModel calculator, string calculate)
+        public ActionResult PackCalculator(CalculatorModel calculator)
         {
-            if (calculate == "add")
-            {
-                if (calculator.CurrentLevel < 21)
-                {
-                    calculator.CurrentPack = calculator.CurrentPack + (calculator.CurrentLevel - 1);
-                    int seizoenPacks = calculator.PlayedSeasons * 5;
-                    int BattlepassPacks = calculator.Battlepass * 12;
-                    calculator.CurrentPack = calculator.CurrentPack + seizoenPacks + BattlepassPacks + calculator.BoughtPacks;
-                    return View(calculator);
-                }
-
-                else if (calculator.CurrentLevel > 21 && calculator.CurrentLevel < 301)
-                {
-                    int CurrentLevelPack = calculator.CurrentLevel - 20;
-                    int PackCalculator2 = (calculator.CurrentLevel / 2);
-                    int CurrentLevelPack2 = PackCalculator2 + 19;
-                    int seizoenPacks = calculator.PlayedSeasons * 5;
-                    int BattlepassPacks = calculator.Battlepass * 12;
-                    calculator.CurrentPack = calculator.CurrentPack + seizoenPacks + BattlepassPacks + calculator.BoughtPacks + CurrentLevelPack2;
-                    return View(calculator);
-
-
-                }
-
-                else if (calculator.CurrentLevel > 300 && calculator.CurrentLevel <= 500)
-                {
-                    int CurrentLevelPack = calculator.CurrentLevel - 300;
-                    int PackCalculator2 = (CurrentLevelPack / 5);
-                    int CurrentLevelPack2 = PackCalculator2 + 159;
-                    int seizoenPacks = calculator.PlayedSeasons * 5;
-                    int BattlepassPacks = calculator.Battlepass * 12;
-                    calculator.CurrentPack = calculator.CurrentPack + seizoenPacks + BattlepassPacks + calculator.BoughtPacks + CurrentLevelPack2;
-                    return View(calculator);
-
-                }
-            }
-            else
-            {
-                return View(calculator);
-            }
+            CalculatorLogic x = new CalculatorLogic();
+            int Y = x.calculator(calculator);
             return View(calculator);
+
         }
     }
 }
---
+++ ApexLogic/CalculatorLogic.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApexLegendsApp.Models;
+using DataLibraryApex.Models;
+using DataLibraryApex.DataAccess;
+
+namespace ApexLogic
+{
+   public class CalculatorLogic
+    {
+        public int calculator(CalculatorModel calculator)
+        {
+            if (calculator.CurrentLevel < 21)
+            {
+                calculator.CurrentPack = calculator.CurrentPack + (calculator.CurrentLevel - 1);
+                int seizoenPacks = calculator.PlayedSeasons * 5;
+                int BattlepassPacks = calculator.Battlepass * 12; 
+                calculator.CurrentPack = calculator.CurrentPack + seizoenPacks + BattlepassPacks + calculator.BoughtPacks;
+                return calculator.CurrentPack;
+            }
+
+            else if (calculator.CurrentLevel > 21 && calculator.CurrentLevel < 301)
+            {
+                int CurrentLevelPack = calculator.CurrentLevel - 20;
+                int PackCalculator2 = (calculator.CurrentLevel / 2);
+                int CurrentLevelPack2 = PackCalculator2 + 19;
+                int seizoenPacks = calculator.PlayedSeasons * 5;
+                int BattlepassPacks = calculator.Battlepass * 12;
+                calculator.CurrentPack = calculator.CurrentPack + seizoenPacks + BattlepassPacks +
+                                         calculator.BoughtPacks + CurrentLevelPack2;
+                return calculator.CurrentPack;
+
+
+            }
+
+            else if (calculator.CurrentLevel > 300 && calculator.CurrentLevel <= 500)
+            {
+                int CurrentLevelPack = calculator.CurrentLevel - 300;
+                int PackCalculator2 = (CurrentLevelPack / 5);
+                int CurrentLevelPack2 = PackCalculator2 + 159;
+                int seizoenPacks = calculator.PlayedSeasons * 5;
+                int BattlepassPacks = calculator.Battlepass * 12;
+                calculator.CurrentPack = calculator.CurrentPack + seizoenPacks + BattlepassPacks +
+                                         calculator.BoughtPacks + CurrentLevelPack2;
+                return calculator.CurrentPack;
+            }
+            else
+            {
+                return 0;
+
+            }
+        }
+    }
+}
